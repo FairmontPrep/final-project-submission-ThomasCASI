b8c93c7c2a817cb522689ff28691ea06e86a7619
haha <hxu730@students.fairmontschools.com>
2025-05-28 10:26:11 -0700

    fixed

--- Client.java
+++ Client.java
@@ -1,17 +1,83 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> test_array_2 = new ArrayList<>();
-        test_array_2.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 0, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 1, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(9, 0, 0, 1, 0, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 2, 0, 0, 0)));
-        test_array_2.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 1, 1, 1)));
-        System.out.println(MazePathFinder.findPath(test_array_2));
+        List<List<Integer>> map = Arrays.asList(
+                Arrays.asList(1, 0, 0, 0, 0, 0),
+                Arrays.asList(0, 0, 0, 0, 0, 0),
+                Arrays.asList(1, 1, 1, 1, 0, 0),
+                Arrays.asList(9, 0, 0, 1, 1, 1),
+                Arrays.asList(0, 1, 1, 1, 0, 0),
+                Arrays.asList(0, 1, 0, 1, 0, 0),
+                Arrays.asList(0, 1, 0, 1, 0, 0),
+                Arrays.asList(0, 1, 0, 1, 0, 0),
+                Arrays.asList(0, 1, 0, 0, 0, 0),
+                Arrays.asList(0, 1, 0, 0, 0, 0)
+        );
+        System.out.println(MazePathFinder.findPath(map));
+    }
+
+    static class MazePathFinder {
+        private static final int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        public static List<String> findPath(List<List<Integer>> map) {
+            int R = map.size();
+            if (R == 0) return List.of();
+            int C = map.getFirst().size();
+
+            List<int[]> starts = new ArrayList<>();
+            for (int c = 0; c < C; c++) {
+                if (map.getFirst().get(c) == 1) starts.add(new int[]{0, c});
+                if (map.get(R - 1).get(c) == 1) starts.add(new int[]{R - 1, c});
+            }
+            for (int r = 0; r < R; r++) {
+                if (map.get(r).getFirst() == 1) starts.add(new int[]{r, 0});
+                if (map.get(r).get(C - 1) == 1) starts.add(new int[]{r, C - 1});
+            }
+
+            for (int[] start : starts) {
+                int sr = start[0], sc = start[1];
+                List<String> path = new ArrayList<>();
+                boolean[][] vis = new boolean[R][C];
+                if (dfs(map, vis, path, sr, sc, sr, sc, -1, false)) {
+                    return path;
+                }
+            }
+
+            return List.of();
+        }
+
+        private static boolean dfs(List<List<Integer>> map, boolean[][] vis, List<String> path, int r, int c, int sr, int sc, int prevDir, boolean turned) {
+            int R = map.size(), C = map.getFirst().size();
+            if (r < 0 || r >= R || c < 0 || c >= C || map.get(r).get(c) != 1 || vis[r][c]) return false;
+            vis[r][c] = true;
+            path.add("A[" + r + "][" + c + "]");
+            if (turned && onWall(r, c, R, C) && !(r == sr && c == sc) && !isOpposite(r, c, sr, sc, R, C)) {
+                return true;
+            }
+            for (int d = 0; d < DIRS.length; d++) {
+                int nr = r + DIRS[d][0], nc = c + DIRS[d][1];
+                boolean newTurn = turned || (prevDir != -1 && prevDir != d);
+                if (dfs(map, vis, path, nr, nc, sr, sc, d, newTurn)) {
+                    return true;
+                }
+            }
+            vis[r][c] = false;
+            path.removeLast();
+            return false;
+        }
+
+        private static boolean onWall(int r, int c, int R, int C) {
+            return r == 0 || r == R - 1 || c == 0 || c == C - 1;
+        }
+
+        private static boolean isOpposite(int r, int c, int sr, int sc, int R, int C) {
+            if (sr == 0 && r == R - 1) return true;
+            if (sr == R - 1 && r == 0) return true;
+            if (sc == 0 && c == C - 1) return true;
+            return sc == C - 1 && c == 0;
+        }
     }
 }
